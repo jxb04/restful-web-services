64ffd6987badbbbd43f43e46d2c72f23dd5545ca
Joel Bernasky <joel_bernasky@hotmail.com>
2020-05-26 18:14:24 -0400

    create java servlet

--- section02/rest-server/src/main/java/com/bernasky/rest/BasicServlet.java
+++ section02/rest-server/src/main/java/com/bernasky/rest/BasicServlet.java
@@ -1,7 +1,15 @@
 package com.bernasky.rest;
 
+import javax.servlet.ServletException:
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class BasicServlet extends HttpServlet   {
-    
+public class BasicServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus
+    }
 }
