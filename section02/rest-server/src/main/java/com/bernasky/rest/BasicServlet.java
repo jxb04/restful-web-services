88ad6c6a9fb9d47c94b153f62997c0708112c83f
Joel Bernasky <joel_bernasky@hotmail.com>
2020-05-27 07:38:34 -0400

    added jetty plugin

--- section02/rest-server/src/main/java/com/bernasky/rest/BasicServlet.java
+++ section02/rest-server/src/main/java/com/bernasky/rest/BasicServlet.java
@@ -1,6 +1,6 @@
 package com.bernasky.rest;
 
-import javax.servlet.ServletException:
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class BasicServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(200);
+        resp.getOutputStream().println("Hello, world!");
     }
 }
