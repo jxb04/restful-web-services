5fc1a33a30767c8fd09aad37e891916e1a17c821
Joel Bernasky <joel_bernasky@hotmail.com>
2020-05-27 09:18:09 -0400

    set up second route

--- section02/rest-server/src/main/java/com/bernasky/rest/SecondServlet.java
+++ section02/rest-server/src/main/java/com/bernasky/rest/SecondServlet.java
@@ -6,17 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class BasicServlet extends HttpServlet {
+public class SecondServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setStatus(200);
-        resp.getOutputStream().println("Hello, world!");
+        resp.getOutputStream().println("Second Servlet");
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setStatus(201);
-        resp.getOutputStream().println("Something was created.");
-    }
 }
